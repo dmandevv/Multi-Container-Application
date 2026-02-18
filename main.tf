591dfef92fba48903204fdf34e6ac4ad2fe24bc8
dmandevv <dmandevv@hotmail.com>
2026-02-18 02:18:59 -0800

    API, Docker, Digital Ocean, Terraform and Ansible setup

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+    required_providers {
+        digitalocean = {
+            source = "digitalocean/digitalocean"
+            version = "~> 2.0"
+        }
+    }
+}
+
+provider "digitalocean" {
+    token = var.do_token
+}
+
+resource "digitalocean_ssh_key" "my_key" {
+    name = "my-desktop-key"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "digitalocean_droplet" "web" {
+    image = "ubuntu-22-04-x64"
+    name = "todo-api-server"
+    region = "sfo3"
+    size = "s-1vcpu-1gb"
+    ssh_keys = [digitalocean_ssh_key.my_key.id]
+}
+
+output "server_ip" {
+  value = digitalocean_droplet.web.ipv4_address
+}
+
+resource "local_file" "ansible_inventory" {
+  content  = <<EOT
+[web_servers]
+${digitalocean_droplet.web.ipv4_address} ansible_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa
+EOT
+  filename = "inventory.ini"
+}
---
+++ src/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Todo = require('./models/todo');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://mongodb:27017/todoapp')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+app.post('/todos', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    });
+    await todo.save();
+    res.status(201).json(todo);
+});
+
+app.get('/todos/:id', async (req, res) => {
+    try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).send('Todo not found');
+    res.json(todo);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.put('/todos/:id', async (req, res) => {
+    try {
+    const todo = await Todo.findByIdAndUpdate(
+    req.params.id, 
+    { title: req.body.title, completed: req.body.completed},
+    { new: true }) // This returns the modified document rather than the original
+    if (!todo) return res.status(404).send('Todo not found');
+    res.json(todo);
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findByIdAndDelete(req.params.id);
+        if (!todo) return res.status(404).send('Todo not found');
+        res.json(todo);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
